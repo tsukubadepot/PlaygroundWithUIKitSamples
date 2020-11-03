f70f26716419880395df62a1376c8cbfc241e8dc
tsukubadepot <tsukubadepot@icloud.com>
2020-11-03 15:18:02 +0900

    add some comments,
    日英両方のコメントを追加した

--- PlaygroundWithUIKitSamples/AlamofireTest.playground/Contents.swift
+++ PlaygroundWithUIKitSamples/AlamofireTest.playground/Contents.swift
@@ -1,7 +1,10 @@
 import UIKit
 import Alamofire
+import PKHUD
 import PlaygroundSupport
 
+// MARK: - GitHub API から得たいデータのうち、必要最低限のものだけ列記
+// Structs to get JSON data from GitHub API
 struct Github: Codable {
     let totalCount: Int
     let incompleteResults: Bool
@@ -26,26 +29,38 @@ struct Item: Codable {
     }
 }
 
+// MARK: - Main View Controller
 class FirstViewController : UIViewController {
+    // UITableView に表示するデータ
+    // didSet + reloadData() で tableView を自動更新
+    // Variable with property observer
     var gitData: [Item] = [] {
         didSet {
             tableView.reloadData()
         }
     }
     
+    // 検索文字列入力用
+    // textField to enter query words
     lazy var textField: UITextField = {
         let tf = UITextField()
         tf.delegate = self
-        tf.placeholder = "input here"
+        tf.placeholder = "Project name, subtitle, etc..."
         tf.borderStyle = .line
+        tf.backgroundColor = .white
         
         return tf
     }()
     
+    // 検索結果の表示
+    // tableView to display query result
     lazy var tableView: UITableView = {
         let tv = UITableView()
         //tv.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
         tv.dataSource = self
+        // UITableView をドラッグさせると自動でキーボードが消える
+        // Allow user to hide keybord by dragging table view
+        tv.keyboardDismissMode = .onDrag
         
         return tv
     }()
@@ -54,29 +69,31 @@ class FirstViewController : UIViewController {
         let view = UIView()
         view.backgroundColor = .systemGray5
         
-        
         view.addSubview(tableView)
         view.addSubview(textField)
+        
         self.view = view
     }
     
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
         
+        // UITextField に対する制約(constraint)
         textField.translatesAutoresizingMaskIntoConstraints = false
         textField.topAnchor.constraint(equalTo: view.topAnchor, constant: 12).isActive = true
         textField.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 12).isActive = true
         textField.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -12).isActive = true
         
+        // UITableView に対する制約(constraint)
         tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.topAnchor.constraint(equalTo: textField.bottomAnchor, constant: 6).isActive = true
         tableView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 12).isActive = true
         tableView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -12).isActive = true
         tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -12).isActive = true
-        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
         view.endEditing(true)
     }
 }
@@ -87,6 +104,9 @@ extension FirstViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 効率を考えると dequeue させるべきだが、今回は標準セルのうち subtitle を使いたいので、あえてインスタンスを毎回生成している
+        // Usually, we need to use dequeueReusableCell method to obtain cell instance,
+        // but in this sample requires default cell with detail view so that  to display subtitle.
         //let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "Cell")
         
@@ -101,28 +121,54 @@ extension FirstViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         
+        // 入力値が空でない場合、検索を実行
         if let text = textField.text, !text.isEmpty {
             searchGitHub(text: text)
         }
+        
         return true
     }
     
+    // MARK: Search GitHub Repository
     func searchGitHub(text: String) {
+        // 入力を空白で分解し、"+" で連結する
+        // split query string by white space and join with "+"
         let query = text.split(separator: " ").joined(separator: "+")
+        
+        // 検索用文字列を連結し、必要に応じてパーセントエンコーディングを行う
+        // conctinationg each strings, and do percent encoding if needed
         if let urlString = ["https://api.github.com/search/repositories?q=",
                             query,
                             "&sort=stars"]
                             .joined()
                             .addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed),
            
-           let url = URL(string: urlString) {
+        let url = URL(string: urlString) {
+            // show Head Up Display
+            // 検索中のポップアップを表示
+            HUD.show(.labeledProgress(title: "Searching...", subtitle: nil))
+            
             AF.request(url).responseData { response in
+                // dismiss HUD
+                // 検索中のポップアップを消す
+                HUD.hide()
+                
                 switch response.result {
                 case .success(let data):
                     do {
                         let result = try
                             JSONDecoder().decode(Github.self, from: data)
                         self.gitData = result.items
+                        
+                        let count = result.items.count
+                        
+                        if count > 0 {
+                            // in case that we can get results
+                            HUD.flash(.labeledSuccess(title: "Success", subtitle: "\(count) respositories were found"), delay: 3)
+                        } else {
+                            // no matching 
+                            HUD.flash(.labeledError(title: "Not found", subtitle: nil), delay: 3)
+                        }
                     } catch {
                         print(error)
                     }
